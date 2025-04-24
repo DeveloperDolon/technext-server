fa414a911ead52980780582b6773d8672bf8b3fa
DeveloperDolon <dolonr718@gmail.com>
2025-04-24 21:28:49 +0600

    app file configured

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,6 @@
-console.log("Server is starting...");
+async function bootstrap() {
+  const app = await import('./app.js');
+  const server = await app.createServer();
+  await server.start();
+  console.log('Server is running on http://localhost:4000');
+}
