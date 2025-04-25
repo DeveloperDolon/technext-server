95835708c37016c032b8239eb1432facfdf2b43e
DeveloperDolon <dolonr718@gmail.com>
2025-04-25 21:36:46 +0600

    project module files added and validation added

---
+++ src/app/modules/Projects/project.validation.ts
@@ -0,0 +1,26 @@
+import { z } from 'zod';
+
+const createProjectZodSchema = z.object({
+  body: z.object({
+    project: z.object({
+      title: z.string().min(1).max(100),
+      budget: z.number().min(0),
+      clientId: z.string(),
+      deadline: z.date(),
+      status: z
+        .enum(['PENDING', 'IN_PROGRESS', 'COMPLETED', 'CANCELLED'])
+        .default('PENDING'),
+    }),
+  }),
+});
+
+const updateProjectZodSchema = z.object({
+  body: z.object({
+    project: createProjectZodSchema.shape.body.shape.project.partial(),
+  }),
+});
+
+export const ProjectValidation = {
+  createProjectZodSchema,
+  updateProjectZodSchema,
+};
