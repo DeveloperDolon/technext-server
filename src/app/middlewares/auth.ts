3dd5109a06d89f8617c42258e8f408492877e314
DeveloperDolon <dolonr718@gmail.com>
2025-04-25 20:37:27 +0600

    auth middleware added

---
+++ src/app/middlewares/auth.ts
@@ -0,0 +1,35 @@
+import httpStatus from 'http-status';
+import { NextFunction, Request, Response } from 'express';
+import ApiError from '../errors/ApiError';
+import { jwtHelpers } from '../helpers/jwtHelpers';
+import config from '../config';
+import { Secret } from 'jsonwebtoken';
+
+const auth = () => {
+  return async (
+    req: Request & { user?: unknown },
+    res: Response,
+    next: NextFunction
+  ) => {
+    try {
+      const token = req.headers.authorization;
+
+      if (!token) {
+        throw new ApiError(httpStatus.UNAUTHORIZED, 'You are not authorized!');
+      }
+
+      const verifiedUser = jwtHelpers.verifyToken(
+        token,
+        config.jwt.secret as Secret
+      );
+
+      req.user = verifiedUser;
+
+      next();
+    } catch (err) {
+      next(err);
+    }
+  };
+};
+
+export default auth;
