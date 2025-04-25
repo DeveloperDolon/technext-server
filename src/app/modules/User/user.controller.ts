2961d3ad38b3053aeebb649834e64e0491065640
DeveloperDolon <dolonr718@gmail.com>
2025-04-25 17:54:08 +0600

    user create servicce method added

---
+++ src/app/modules/User/user.controller.ts
@@ -0,0 +1,10 @@
+import catchAsync from "../../utils/catchAsync";
+
+
+const createUser = catchAsync(async (req, res) => {
+    const resule = 
+});
+
+export const userController = {
+    createUser
+};
---
+++ src/app/modules/User/user.service.ts
@@ -0,0 +1,20 @@
+import { User } from '@prisma/client';
+import { Request } from 'express';
+import * as bcrypt from 'bcrypt';
+import prisma from '../../utils/prisma';
+
+const createUserIntoDB = async (req: Request): Promise<User> => {
+  const hashedPassword: string = await bcrypt.hash(req.body.password, 12);
+
+  const user = {
+    ...req.body.user,
+    password: hashedPassword,
+  };
+
+  const result = await prisma.user.create(user);
+  return result;
+};
+
+export const userService = {
+  createUserIntoDB,
+};
